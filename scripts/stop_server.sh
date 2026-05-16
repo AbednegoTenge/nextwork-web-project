f346bcb8f120806f3edb147233b5e611e94262f5
EC2 Default User <ec2-user@ip-172-31-34-193.eu-west-3.compute.internal>
2026-05-16 22:43:31 +0000

    rollback

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+SYSTEMCTL=/usr/bin/systemctl
+
 isExistApp="$(pgrep httpd)"
 if [[ -n $isExistApp ]]; then
-sudo systemctl stop httpd.service
+    $SYSTEMCTL stop httpd.service
 fi
+
 isExistApp="$(pgrep tomcat)"
 if [[ -n $isExistApp ]]; then
-sudo systemctl stop tomcat.service
+    $SYSTEMCTL stop tomcat.service
 fi
